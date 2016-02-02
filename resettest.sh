1e5c1f3b12221248f0663e8e1b117bf0654a7818
hbh7 <newhbh7@gmail.com>
2016-02-02 13:00:45 -0500

    Works now :D

--- resettest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf test/*
-touch test/LASTNAME_lel.docx
-touch test/haha.docx
--- victore.py
+++ victore.py
@@ -5,50 +5,45 @@
 
 import os, configparser, shutil, string, errno, glob
 
-print("Reading mullen_cfg.txt for configuration...")
+print("Reading victore_cfg.txt for configuration...")
 
 config = configparser.ConfigParser()
 config.read(os.path.expanduser('victore_cfg.txt'))
 
-Settings = config['Mullen']
-oldDocDir = Settings['newDocDir']
-newDocDir1 = Settings['oldDocDir']
+Settings = config['Victore']
+newDocDir1 = Settings['newDocDir']
+oldDocDir = Settings['oldDocDir']
 firstName = Settings['firstName'].capitalize()
 lastName = Settings['lastName'].upper()
 className = Settings['className']
+classDirName = Settings['classDirName']
 
-# newDocDir2 = newDocDir1 + firstName + "_" + lastName + "_" + className
 newDocDir2 = firstName + "_" + lastName + "_" + className
-
-print ("Creating directory if not already existing.")
-if os.path.isdir(newDocDir2):
-    print ("Directory exists already")
-else:
-    print ("Directory does not exist, creating")
-    os.mkdir(newDocDir2)
 
 print("First name: " + firstName)
 print("Last name: " + lastName)
 print("Files to be converted are in: " + oldDocDir)
-print("Files after conversion are in: " + newDocDir2)
+print("Files after conversion are in: " + newDocDir1)
 print("Preparing to fix files...")
 
-for dirpath, dirnames, files in os.walk(oldDocDir):
-    print ("dirpath", dirpath)
-    print ("dirnames", dirnames)
-    print("files", files)
+for dirpath, dirnames, files in os.walk(oldDocDir, topdown=True):
+    for dir in dirnames:
+        makeDir = os.path.join(dirpath, dir)
+        makeDir = makeDir.replace("Harris", classDirName)
+        print ("makeDir: ",makeDir)
+        if not os.path.isdir(makeDir):
+            os.mkdir(makeDir)
+    
     for name in files:
-        print(os.path.join(dirpath, name))
         fileName = os.path.join(dirpath, name)
-
-        # oldName = oldDocDir + "/" + fileName
-        # newName = newDocDir2 + "/"  + lastName + "_" + fileName
         newName = fileName.replace("Harris", newDocDir2)
+        newName = newName.replace(name, (firstName.lower() + "_" + lastName.upper() + "_" + name))
         print(fileName + " -> " + newName)
+        
         try:
             shutil.copytree(fileName, newName)
         except OSError as exc:
             if exc.errno == errno.ENOTDIR:
                 shutil.copy(fileName, newName)
         else: raise
-	
+    print ("")
